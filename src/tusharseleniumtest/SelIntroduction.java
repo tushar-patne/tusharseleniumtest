29ef1577d45b1c9efcdb54faa4dc6528102bd988
tushar-patne <tushar-patne@users.noreply.github.com>
2024-07-23 09:52:40 +0530

    test commit

---
+++ src/tusharseleniumtest/SelIntroduction.java
@@ -0,0 +1,21 @@
+package tusharseleniumtest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SelIntroduction {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://google.com");
+		
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
+		
+		driver.quit();
+
+	}
+
+}
